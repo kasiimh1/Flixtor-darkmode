364076a8ad13ebaab3ef50f975d393ac2cab481b
Kasim Hussain <kasiimh1@users.noreply.github.com>
2019-01-10 16:55:35 +0000

    Add files via upload

---
+++ Flixtor.to Dark Mode.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         Flixtor.to DarkMode
+// @namespace    https://github.com/kasiimh1
+// @version      0.1
+// @description  Dark themes > light themes, enjoy!
+// @author       Kasim Hussain
+// @match        https://flixtor.to/*
+// @grant        GM_addStyle
+// ==/UserScript==
+
+var darkMode=`
+
+body, .card{
+background-color: #000 !important;
+color:white !important;
+}
+
+.main .card.back, .main .card.front, .show .card.back, .show .card.front {
+ box-shadow: 0.2rem 0.2rem 1rem #17a2b8;
+}
+.section-watch-season {
+background-color: rgba(0,0,0,0.7);
+}
+
+`
+GM_addStyle(darkMode);
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Flixtor-darkmode
-Dark Mode Theme for Flixtor.to
+# Flextor-darkmode
+
+Fully functional Dark Mode for Flixtor.to.
+
+Install TamperMonkey - https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=en
+
+Install Flextor.to DarkMode script with this URL - https://github.com/kasiimh1/Flixtor-darkmode/blob/master/Flixtor.to%20Dark%20Mode.user.js
+
+Let me know if i broke anything via the Issues tab
+
+Will update this if Flixtor decides to break something in the future.
+
+~ Kasim
