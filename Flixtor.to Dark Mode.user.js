891b234390899b9b0a368bd8db2d8db4f873b294
Kasim Hussain <kasiimh1@gmail.com>
2019-05-21 10:41:58 +0100

    Update Flixtor.to Dark Mode.user.js

--- Flixtor.to Dark Mode.user.js
+++ Flixtor.to Dark Mode.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Flixtor.to DarkMode
 // @namespace    https://github.com/kasiimh1
-// @version      0.1
+// @version      0.1.1
 // @description  Dark themes > light themes, enjoy!
 // @author       Kasim Hussain
 // @match        https://flixtor.to/*
@@ -18,8 +18,9 @@ color:white !important;
 .main .card.back, .main .card.front, .show .card.back, .show .card.front {
  box-shadow: 0.2rem 0.2rem 1rem #17a2b8;
 }
-.section-watch-season {
+.section-watch-season, .table-episodes {
 background-color: rgba(0,0,0,0.7) !important;
+color: white !important;
 }
 
 `
